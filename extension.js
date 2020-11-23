99ead73b7e7b8eb706d47e5170d155ae0b724a4d
dbroemme <darren.broemmer@gmail.com>
2020-11-23 18:30:37 -0500

    Minor improvements Added some primitives for messaging Got a simple message pass to work

--- extension.js
+++ extension.js
@@ -13,8 +13,7 @@ function activate(context) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('The "yesyoucancoderuby" extension is now active!');
-	console.log("[1] The root dir is " + rootDir);
+	console.log("The yesyoucancoderuby extension is now active. The root dir is " + rootDir);
 	var chosenExampleNumber = 'none';
 
 	// The code you place here will be executed every time your command is executed
@@ -41,15 +40,15 @@ function activate(context) {
 		message => {
 		  switch (message.command) {
 			case 'run':
-			  console.log("The run button was clicked");
 			  runRubyProgram(currentPanel, rootDir, chosenExampleNumber, fs);
 			  return;
 			case 'start':
 			  chosenExampleNumber = message.text;
 			  openExample(currentPanel, message.text, rootDir, fs);
 			  return;
-			case 'alert':
-			  vscode.window.showErrorMessage(message.text);
+			case 'gets':
+			  //vscode.window.showErrorMessage(message.text);
+			  push(message.text);
 			  return;
 		  }
 		},
@@ -67,11 +66,30 @@ function activate(context) {
 	context.subscriptions.push(disposable);
 
 	hideTerminalWindow();
-	vscode.commands.executeCommand("workbench.action.closeSidebar");
+	//vscode.commands.executeCommand("workbench.action.closeSidebar");
+	//vscode.commands.executeCommand("workbench.action.toggleActivityBarVisibility");
 
 	var mapHtml = fs.readFileSync(context.extensionPath + "/problems/map.html", 'utf8');
-	//console.log("the map html is: " + mapHtml);
-    currentPanel.webview.postMessage({ challengemap: mapHtml });
+	currentPanel.webview.postMessage({ challengemap: mapHtml });
+	
+	var fileSystemWatcher = vscode.workspace.createFileSystemWatcher("**/{r,j}*");
+    fileSystemWatcher.ignoreCreateEvents = false;
+    fileSystemWatcher.ignoreChangeEvents = true;
+    fileSystemWatcher.ignoreDeleteEvents = true;
+    fileSystemWatcher.onDidCreate((e) => {
+		// Get just the filename
+		var lastDelimIndex = e.path.lastIndexOf('/');
+		var watchedFileName = e.path.slice(lastDelimIndex + 1);
+		//vscode.window.showInformationMessage("The watched file name is " + watchedFileName);
+		if (watchedFileName.startsWith("r")) {
+			//vscode.window.showInformationMessage("A Ruby message file was created: " + e);
+			getWebviewInput(currentPanel);
+		} else if (watchedFileName.startsWith("j")) {
+			vscode.window.showInformationMessage("This was our own file that was created: " + e);
+		} else {
+			vscode.window.showInformationMessage("An unidentified file was created: " + e);
+		} 
+    });
 }
 exports.activate = activate;
 
@@ -94,40 +112,53 @@ function openExample(currentPanel, exampleNumber, rootDir, fs) {
 function runRubyProgram(currentPanel, rootDir, chosenExampleNumber, fs) {
 	console.log("In runRubyProgram() for example " + chosenExampleNumber);
 	console.log("[3] The root dir is " + rootDir);
-	// TODO auto save the current editor file, if the user has not done so already
-	// because we need the file itself to be saved for it to be run by the
-	// next command
+	// Auto save the current editor file, if the user has not done so already
+	// because we need the file itself to be saved on disk for the run command
+	let openTextDocuments = vscode.window.visibleTextEditors;
+	openTextDocuments.forEach(textDoc => {
+		if (textDoc.document.fileName.endsWith(chosenExampleNumber + ".rb")) {
+			textDoc.document.save();
+		}
+	});
+	
 	let editorFileName = rootDir + "/problems/code" + chosenExampleNumber + ".rb";
 	let commandString = "ruby " + editorFileName;
 	console.log("Preparing to run ruby using command: " + commandString);
 
-	try {
-		const { exec } = require("child_process");
-		exec("ruby " + editorFileName, (error, stdout, stderr) => {
-			if (error) {
-				console.log(`error: ${error.message}`);
-				return;
-			}
-			if (stderr) {
-				console.log(`stderr: ${stderr}`);
-				return;
-			}
-			console.log("---");
-			console.log(stdout);
-			let parsedOutput = parseOutput(stdout);
-			console.log("---");
-			console.log(parsedOutput);
-			// TODO Add the answer and compare what we got with the answer
+	var expectedOutputStr = fs.readFileSync(rootDir + "/problems/answer" + chosenExampleNumber + ".txt", 'utf8');
+	var expectedOutput = expectedOutputStr.split(/\r?\n/);
 
-			// Here we send info back to the webview for display
-			currentPanel.webview.postMessage(
-				{ output: parsedOutput["output"], feedback: parsedOutput["feedback"] });
+	try {
+		const { spawn } = require("child_process");
+		
+		const rb = spawn('ruby', [editorFileName]);
+        rb.stdout.on('data', (data) => {
+			console.log("ruby stdout: " + data);
+			let parsedOutput = parseOutput(data);
+			console.log("The parsed output is: " + parsedOutput);
+			let feedback = determineFeedback(parsedOutput["asarray"], expectedOutput);
+			console.log("The feedback is: " + feedback);
+			// Send info back to the webview for display
+		  	currentPanel.webview.postMessage(
+		 		{ output: parsedOutput["output"], feedback: feedback });
+        });
+        rb.stderr.on('data', (data) => {
+            console.error("ruby stderr: " + data);
+        });
+        rb.on('close', (code) => {
+            console.log("Your ruby program exited with code " + code);
 		});
+
 	} catch (err) {
 		console.log("We caught an exception");
 		console.log(err);
 	}
 	//setTimeout(function(){ getCommandOutput(currentPanel, chosenExampleNumber); }, 1000);
+}
+
+function getWebviewInput(currentPanel) {
+	currentPanel.webview.postMessage({ input: "gets" });
+	// TODO Do we need to wait now. I think so, right?
 }
 
 function hideTerminalWindow() {
@@ -193,13 +224,22 @@ function getWebviewContent(imageUri) {
         </tr>
       </table>
 	  <br/>
+
+	  <!-- Form to get string input -->
+	  <div id="getsdiv" style="display: none;">
+	  <label for="getsinput">Program Input:</label>
+	  <input type="text" id="getsinput" name="getsinput">
+	  <button onClick="sendGetsInput()">Enter</button> 
+	  </div>
+
 	  <div id="challenge">&nbsp;</div>
 	  <br/><b>Output</b><div>
-	  <pre id="output">Click 'Run your code' to see results here</pre>
+	  <pre id="output"> </pre>
 	  <br/></div>
 	  <br/><b>Feedback</b><div>
-	  <pre id="feedback">You will get feedback on your output here.</pre>
+	  <pre id="feedback"> </pre>
 	  <br/></div>
+
 	  </div>  <!-- end workarea -->
 	  <script>
 		  const vscode = acquireVsCodeApi();
@@ -221,6 +261,17 @@ function getWebviewContent(imageUri) {
 		  function reset() {
 			document.getElementById("challengemap").style.display = "block";
 			document.getElementById("workarea").style.display = "none";
+			document.getElementById("output").innerHTML = " ";
+			document.getElementById("feedback").innerHTML = " ";
+		  }
+		  function sendGetsInput() {
+			getsInput = document.getElementById("getsinput").value;
+			vscode.postMessage({
+				command: 'gets',
+				text: getsInput
+			})
+			document.getElementById("getsinput").value = "";
+			document.getElementById("getsdiv").style.display = "none";
 		  }
 
 		  // Handle the message inside the webview
@@ -238,6 +289,9 @@ function getWebviewContent(imageUri) {
 			  if (message.challenge) {
 				document.getElementById("challenge").innerHTML = message.challenge;
 			  }
+			  if (message.input) {
+				document.getElementById("getsdiv").style.display = "block";
+			  }
 		   });
 	  </script>
   </body>
@@ -249,7 +303,9 @@ function getWebviewContent(imageUri) {
 function deactivate() {}
 
 function parseOutput(strOutput) {
-	let lines = strOutput.split(/\r?\n/)
+	console.log("in parseOutput with " + strOutput + "." + strOutput.constructor.name + ".");
+	let lines = strOutput.toString('utf8').split(/\r?\n/)
+	console.log("got lines: " + lines);
 	var actualOutputLines = []
 	lines.forEach(element => {
 		console.log(element);
@@ -264,8 +320,109 @@ function parseOutput(strOutput) {
 	// TODO Add a hint button to provide a template
 	return {
 		output: actualOutputLines.join("<br/>"),
-		feedback: "There are " + actualOutputLines.length + " lines of output"
+		asarray: actualOutputLines
 	};
+}
+
+function determineFeedback(parsedOutput, expectedOutput) {
+	console.log("in determineFeedback. Actual length: " + parsedOutput.length + "  Expected length:" + expectedOutput.length);
+	console.log("---------  Actual ------------");
+	console.log(parsedOutput);
+	console.log("--------- Expected -----------");
+	console.log(expectedOutput);
+	console.log("------------------------------");
+	var actualCount = 0;
+	var expectedOutputCount = 0;
+	var done = false;
+	var match = false;
+	while (expectedOutputCount < expectedOutput.length && !done) {
+		console.log("Comparing lines " + actualCount + ", " + expectedOutputCount);
+		let expectedLine = expectedOutput[expectedOutputCount].replace(/[\n\r]+/g, '');
+		let actualLine = parsedOutput[actualCount];
+		console.log("[" + actualLine + "], [" + expectedLine + "]");
+		let comparison = actualLine.indexOf(expectedLine);
+		console.log("Comparison value: " + comparison);
+		actualCount = actualCount + 1;
+		if (comparison == -1) {
+			console.log("Line " + actualCount + " is not a match.");
+			if (actualCount >= parsedOutput.length) {
+				console.log("We should be done.");
+				done = true;
+			}
+		} else {
+			console.log("Line " + actualCount + " has a match.");
+			expectedOutputCount = expectedOutputCount + 1;
+			if (expectedOutputCount >= expectedOutput.length) {
+				match = true;
+			}
+		}
+	}
+	if (match) {
+		console.log("We got a match");
+		return "We got a match";
+	}
+	console.log("No match");
+	return "No match";
+}
+
+
+//
+// This is file-based messaging logic
+//
+var write_count = 1;
+var read_count = 1;
+
+function resetMessageQueues() {
+	write_count = 1;
+	read_count = 1;
+}
+
+function push(obj) {
+  let file_name = rootDir + "/problems/j" + write_count.toString();
+  if (fs.existsSync(file_name)) {
+    throw file_name + " already exists, cannot write";
+  }
+  write_count = write_count + 1;
+  fs.writeFile(file_name,
+      obj, (err) => {
+      if (err) throw err;
+  });
+}
+
+// function pop() {
+//   let file_name = rootDir + "/problems/r" + read_count.toString();
+//   console.log("Attempting to read from file " + file_name);
+//   if (fs.existsSync(file_name)) {
+//     console.log("file does exist");
+//     var d = new Date();
+//     var n = d.getTime();
+//     let s = fs.statSync(file_name);
+//     let diff = n - s.mtimeMs;
+//     console.log("The file is " + diff + " ms old");
+//     if (diff < 500) {
+//       // The file is too new, pretend it isn't there yet
+//       return null;
+//     }
+//     read_count = read_count + 1;
+//     const data = fs.readFileSync(file_name, 'utf8');
+//     console.log("The file data is " + data + ".");
+//     return data;
+//   } else {
+//     console.log("file does not exist");
+//   }
+//   return null;
+// }
+
+function timeout() {
+    setTimeout(function () {
+        var o = pop();
+        if (o == null) {
+          // we are done
+        } else {
+          console.log("output: " + o);
+          timeout();
+        }
+    }, 1000);
 }
 
 module.exports = {
